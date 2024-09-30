853e919f12f18fb008d1b77532dd543e2ba67fab
rohitsinghme1 <rohitsinghme1@gmail.com>
2024-09-30 23:05:34 +0530

    generic task 11

--- JAVA/gentask1.java
+++ JAVA/gentask1.java
@@ -1,20 +1,20 @@
-class gentask1<T> {
+class gentask1<T extends Number> {
 
     T data, data1;
+    Double sum;
 
     public void Display(T data, T data1) {
         this.data = data;
         this.data1 = data1;
 
-        System.out.println(data);
-        System.out.println(data1);
+        sum = (data.doubleValue() + data1.doubleValue());
+        System.out.println(sum);
 
     }
 
-    public static void main(String arghs[]) {
+    public static void main(String args[]) {
         gentask1<Integer> g = new gentask1<>();
-        g.Display(4, 3);
-        gentask1<String> gg = new gentask1<>();
-        gg.Display("Rohit", "Singh");
+        g.Display(4, 5);
+
     }
 }
