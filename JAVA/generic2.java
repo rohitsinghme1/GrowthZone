c95684d5b5b2aa96319da428a08b0911ea80e895
rohitsinghme1 <rohitsinghme1@gmail.com>
2024-11-05 21:49:37 +0530

    geneic bounded

--- JAVA/generic2.java
+++ JAVA/generic2.java
@@ -1,12 +1,12 @@
-class gent<T extends Number> {
-    public void display() {
-        System.out.println("This is a bounded type generics class");
-    }
-}
-
 class generic2 {
     public static void main(String args[]) {
         gent<Integer> g = new gent<>();
         g.display();
+    }
+}
+
+class gent<T extends Number> {
+    public void display() {
+        System.out.println("This is a bounded type generics class");
     }
 }
