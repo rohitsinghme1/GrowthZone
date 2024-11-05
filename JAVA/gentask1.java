f88cd5b41e5c30e7cb881ea6862ef232df945308
rohitsinghme1 <rohitsinghme1@gmail.com>
2024-11-05 22:00:17 +0530

    generic bounded

--- JAVA/gentask1.java
+++ JAVA/gentask1.java
@@ -3,18 +3,20 @@ class gentask1<T extends Number> {
     T data, data1;
     Double sum;
 
-    public void Display(T data, T data1) {
+    public gentask1(T data, T data1) {
         this.data = data;
         this.data1 = data1;
+    }
 
+    public void display() {
         sum = (data.doubleValue() + data1.doubleValue());
         System.out.println(sum);
 
     }
 
     public static void main(String args[]) {
-        gentask1<Integer> g = new gentask1<>();
-        g.Display(4, 5);
+        gentask1<Integer> g = new gentask1<>(4, 5);
+        g.display();
 
     }
 }
